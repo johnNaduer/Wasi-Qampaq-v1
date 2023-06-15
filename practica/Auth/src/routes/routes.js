c0b1649d42165b97cfc6585c0d71d344eef0ea9c
Piero2023 <estibensmax2022@gmail.com>
2023-06-15 08:35:10 -0500

    adding routes

--- practica/Auth/src/routes/routes.js
+++ practica/Auth/src/routes/routes.js
@@ -10,7 +10,22 @@ const routes = [
     path: "/register",
     name: "register",
     component: () => import("../pages/RegisterView.vue"),
-  }
+  },
+  {
+    path: "/crear",
+    name: "crear",
+    component: () => import("../pages/CreateView.vue"),
+  },
+  {
+    path: "/editar",
+    name: "editar",
+    component: () => import("../pages/EditarView.vue"),
+  },
+  {
+    path: "/listar",
+    name: "listar",
+    component: () => import("../pages/ListarView.vue"),
+  },
 ];
 
 const router = createRouter({
