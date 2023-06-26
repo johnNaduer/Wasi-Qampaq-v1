45313e2d818e11769beaece246763f896633dfeb
Piero2023 <estibensmax2022@gmail.com>
2023-06-26 13:25:51 -0500

    adding theme to app.vue

--- practica/Auth/src/main.js
+++ practica/Auth/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import './style.css'
 import router from './routes/routes'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 createApp(App).use(router).mount('#app')
-
