b4ea63e3ad6ada08101a128d83d28d740f97db10
johnNaduer <naduer789@gmail.com>
2023-07-03 09:14:58 -0500

    feat: update code

---
+++ practica/Auth/src/components/Boton.vue
@@ -0,0 +1,39 @@
+<template>
+  <Bar
+    id="my-chart-id"
+    :options="chartOptions"
+    :data="chartData"
+  />
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data() {
+    return {
+      chartData: {
+        labels: [ 'January', 'February', 'March' ],
+        datasets: [ { data: [40, 20, 12],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.8)',  // Color para la primera barra
+            'rgba(54, 162, 235, 0.8)', // Color para la segunda barra
+            'rgba(75, 192, 192, 0.8)', // Color para la tercera barra
+          ],
+        barThickness: 70,
+        },
+
+        ]
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
+  }
+}
+</script>
